18ad2482a651daf3834da50b515eeb8c568ddf0a
ma-sadeghi <amin.sadeghi@live.com>
2026-05-21 23:43:49 +0000

    Deploying to gh-pages from @ ma-sadeghi/ma-sadeghi.github.io@55e8f547c5b634236a9ffa3e03e76972ed969a9f 🚀

---
+++ assets/js/giscus-setup.js
@@ -0,0 +1,42 @@
+function determineGiscusTheme() {
+  
+    let theme =
+      localStorage.getItem("theme") ||
+      document.documentElement.getAttribute("data-theme") ||
+      "system";
+
+    if (theme === "dark") return "dark";
+    if (theme === "light") return "light";
+
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return prefersDark ? "dark" : "light";
+  
+}
+
+(function setupGiscus() {
+  let giscusTheme = determineGiscusTheme();
+
+  let giscusAttributes = {
+    src: "https://giscus.app/client.js",
+    "data-repo": "ma-sadeghi/ma-sadeghi.github.io",
+    "data-repo-id": "R_kgDOJ8V4-g",
+    "data-category": "Comments",
+    "data-category-id": "DIC_kwDOJ8V4-s4CX7xH",
+    "data-mapping": "title",
+    "data-strict": "1",
+    "data-reactions-enabled": "1",
+    "data-emit-metadata": "0",
+    "data-input-position": "bottom",
+    "data-theme": giscusTheme,
+    "data-lang": "en",
+    crossorigin: "anonymous",
+    async: true,
+  };
+
+  let giscusScript = document.createElement("script");
+  Object.entries(giscusAttributes).forEach(([key, value]) =>
+    giscusScript.setAttribute(key, value)
+  );
+  document.getElementById("giscus_thread").appendChild(giscusScript);
+})();
+
