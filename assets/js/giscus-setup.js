ee7159304f4a380fc52d5fff1a7f36e4ccbcfa9b
ma-sadeghi <amin.sadeghi@live.com>
2026-05-21 23:44:11 +0000

    Deploying to gh-pages from @ ma-sadeghi/ma-sadeghi.github.io@d527315c604bbc765bf544c27333830b5319216b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -19,9 +19,9 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "ma-sadeghi/ma-sadeghi.github.io",
-    "data-repo-id": "R_kgDOJ8V4-g",
+    "data-repo-id": "",
     "data-category": "Comments",
-    "data-category-id": "DIC_kwDOJ8V4-s4CX7xH",
+    "data-category-id": "",
     "data-mapping": "title",
     "data-strict": "1",
     "data-reactions-enabled": "1",
